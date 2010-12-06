adb92113991e45c916d82bfcfa20376a614ec4cd
lance@indiana.edu <lance@indiana.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-12-06 20:07:57 +0000

    HYB-69 Unit test coverage for new BL5 functioanlity
    git-svn-id: https://source.sakaiproject.org/svn/hybrid/trunk@85991 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- hybrid/src/test/org/sakaiproject/hybrid/tool/SitesServletTest.java
+++ hybrid/src/test/org/sakaiproject/hybrid/tool/SitesServletTest.java
@@ -45,19 +45,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.sakaiproject.api.app.messageforums.SynopticMsgcntrItem;
 import org.sakaiproject.api.app.messageforums.SynopticMsgcntrManager;
 import org.sakaiproject.authz.api.Member;
 import org.sakaiproject.component.api.ComponentManager;
 import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.entity.api.EntityPropertyNotDefinedException;
+import org.sakaiproject.entity.api.EntityPropertyTypeException;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.user.api.Preferences;
 import org.sakaiproject.user.api.PreferencesService;
 import org.sakaiproject.user.api.User;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SitesServletTest {
+	private static final String UID = "admin";
+	private static final String EID = UID;
+
 	@Mock
 	protected SitesServlet sitesServlet;
 	@Mock
@@ -80,14 +89,22 @@ public class SitesServletTest {
 	protected ServletConfig config;
 	@Mock
 	protected Session session;
-	@Mock
+	@Mock(name = "!admin")
 	protected Site site;
+	@Mock(name = "My Workspace")
+	protected Site myWorkSpace;
 	@Mock
 	protected User user;
 	@Mock
 	protected Set<Member> members;
 	@Mock
 	protected PrintWriter writer;
+	@Mock
+	protected Preferences preferences;
+	@Mock
+	protected ResourceProperties resourceProperties;
+	@Mock
+	SynopticMsgcntrItem synopticMsgcntrItem1;
 
 	@BeforeClass
 	public static void beforeClass() {
@@ -106,7 +123,8 @@ public class SitesServletTest {
 	@Before
 	public void setUp() throws Exception {
 		when(sessionManager.getCurrentSession()).thenReturn(session);
-		when(session.getUserEid()).thenReturn("admin");
+		when(session.getUserEid()).thenReturn(EID);
+		when(sessionManager.getCurrentSessionUserId()).thenReturn(UID);
 		when(site.getTitle()).thenReturn("Administration Workspace");
 		when(site.getId()).thenReturn("!admin");
 		when(site.getUrl())
@@ -131,6 +149,13 @@ public class SitesServletTest {
 								null,
 								org.sakaiproject.site.api.SiteService.SortType.TITLE_ASC,
 								null)).thenReturn(siteList);
+		when(siteService.getUserSiteId(UID)).thenReturn("~admin");
+		when(siteService.getSite("~admin")).thenReturn(myWorkSpace);
+		when(myWorkSpace.getId()).thenReturn("~admin");
+		when(preferencesService.getPreferences(UID)).thenReturn(preferences);
+		when(preferences.getProperties("sakai:portal:sitenav")).thenReturn(
+				resourceProperties);
+		when(resourceProperties.getLongProperty("tabs")).thenReturn(11L);
 		when(response.getWriter()).thenReturn(writer);
 		when(componentManager.get(SessionManager.class)).thenReturn(
 				sessionManager);
@@ -141,7 +166,17 @@ public class SitesServletTest {
 				synopticMsgcntrManager);
 		when(componentManager.get(PreferencesService.class)).thenReturn(
 				preferencesService);
+		when(request.getParameter(SitesServlet.CATEGORIZED)).thenReturn("true");
+		when(request.getParameter(SitesServlet.UNREAD)).thenReturn("true");
+		when(request.getParameter(SitesServlet.LOCALE)).thenReturn("en_US");
 		when(request.getLocale()).thenReturn(Locale.getDefault());
+		when(synopticMsgcntrItem1.getSiteId()).thenReturn("!admin");
+		when(synopticMsgcntrItem1.getNewForumCount()).thenReturn(7);
+		when(synopticMsgcntrItem1.getNewMessagesCount()).thenReturn(13);
+		final List<SynopticMsgcntrItem> synopticMsgcntrItems = new ArrayList<SynopticMsgcntrItem>();
+		synopticMsgcntrItems.add(synopticMsgcntrItem1);
+		when(synopticMsgcntrManager.getWorkspaceSynopticMsgcntrItems(UID))
+				.thenReturn(synopticMsgcntrItems);
 		sitesServlet = new SitesServlet();
 		sitesServlet.setupTestCase(componentManager);
 		sitesServlet.init(config);
@@ -151,7 +186,83 @@ public class SitesServletTest {
 	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
 	 */
 	@Test
-	public void testNormalBehavior() {
+	public void testDoGetNormalBehavior() {
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetNullEid() {
+		when(session.getUserEid()).thenReturn(null);
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetEmptyEid() {
+		when(session.getUserEid()).thenReturn("");
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetLocaleLanguage() {
+		when(request.getParameter(SitesServlet.LOCALE)).thenReturn("es");
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetLocaleWithVariant() {
+		when(request.getParameter(SitesServlet.LOCALE)).thenReturn(
+				"es_ES_Traditional");
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetLocaleWithVariant2() {
+		/*
+		 * While multiple variants is not currently supported in the get
+		 * parameter parser, they should not break anything.
+		 */
+		when(request.getParameter(SitesServlet.LOCALE)).thenReturn(
+				"es_ES_Traditional_MAC");
 		try {
 			sitesServlet.doGet(request, response);
 			verify(response).setStatus(HttpServletResponse.SC_OK);
@@ -226,6 +337,39 @@ public class SitesServletTest {
 	}
 
 	/**
+	 * Tests {@link SitesServlet#init(ServletConfig)}
+	 */
+	@Test
+	public void testInitNullSynopticMsgcntrManager() {
+		when(componentManager.get(SynopticMsgcntrManager.class)).thenReturn(
+				null);
+		try {
+			sitesServlet.init(config);
+			fail("IllegalStateException should be thrown");
+		} catch (IllegalStateException e) {
+			assertNotNull(e);
+		} catch (Throwable e) {
+			fail("Throwable should not be thrown");
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#init(ServletConfig)}
+	 */
+	@Test
+	public void testInitNullPreferencesService() {
+		when(componentManager.get(PreferencesService.class)).thenReturn(null);
+		try {
+			sitesServlet.init(config);
+			fail("IllegalStateException should be thrown");
+		} catch (IllegalStateException e) {
+			assertNotNull(e);
+		} catch (Throwable e) {
+			fail("Throwable should not be thrown");
+		}
+	}
+
+	/**
 	 * Tests {@link SitesServlet#setupTestCase(ComponentManager)}
 	 */
 	@Test
@@ -237,6 +381,82 @@ public class SitesServletTest {
 			assertNotNull(e);
 		} catch (Throwable e) {
 			fail("Throwable should not be thrown");
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testEntityPropertyNotDefinedException() {
+		try {
+			when(resourceProperties.getLongProperty("tabs")).thenThrow(
+					new EntityPropertyNotDefinedException());
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testEntityPropertyTypeException() {
+		try {
+			when(resourceProperties.getLongProperty("tabs")).thenThrow(
+					new EntityPropertyTypeException("message"));
+			sitesServlet.doGet(request, response);
+			fail("IllegalStateException should be thrown");
+		} catch (IllegalStateException e) {
+			assertNotNull("IllegalStateException should be thrown: " + e, e);
+		} catch (Throwable e) {
+			fail("IllegalStateException should be thrown");
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testNullPreferences() {
+		when(preferencesService.getPreferences(UID)).thenReturn(null);
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testIdUnusedException() {
+		try {
+			when(siteService.getSite("~admin")).thenThrow(
+					new IdUnusedException("message"));
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
+		}
+	}
+
+	/**
+	 * Tests {@link SitesServlet#doGet(HttpServletRequest, HttpServletResponse)}
+	 */
+	@Test
+	public void testDoGetNotCategorized() {
+		when(request.getParameter(SitesServlet.CATEGORIZED))
+				.thenReturn("false");
+		try {
+			sitesServlet.doGet(request, response);
+			verify(response).setStatus(HttpServletResponse.SC_OK);
+		} catch (Throwable e) {
+			assertNull("Exception should not be thrown: " + e, e);
 		}
 	}
 
